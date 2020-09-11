029a39c3a0e41cbb7e295354b4a9c947bc3a75a7
Shuyang He <maxhe2019@gmail.com>
2020-09-10 21:36:04 -0700

    09/10

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
-import "./Navigation.scss";
+import "./Navigation.css";
 import data from "./NavigationData.js";
 import NavLink from "../NavLink/NavLink";
 import { showSideNav } from "../../actions/SideNavDisplay";
